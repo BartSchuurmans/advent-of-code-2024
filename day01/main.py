1ebacc7f3ac39811d61369e9c1ddfa3e54f028be
Bart Schuurmans <bart@minnozz.com>
2024-12-14 12:52:22 +0100

    Replace day01part1 with native Python implementation

--- day01/main.py
+++ day01/main.py
@@ -1,14 +1,9 @@
-import polars as pl
 from pathlib import Path
 
+with open(Path(__file__).parent / "input.txt") as f:
+    xys = [line.split() for line in f]
+    xs = sorted(int(xy[0]) for xy in xys)
+    ys = sorted(int(xy[1]) for xy in xys)
 
-df = pl.read_csv(
-    Path(__file__).parent / "input.txt",
-    has_header=False,
-    separator=" ",
-).select(left=pl.col("column_1"), right=pl.col("column_4"))
-
-total_distance = df.select(
-    (pl.col("left").sort() - pl.col("right").sort()).abs().sum()
-).item()
+total_distance = sum(abs(x - y) for x, y in zip(xs, ys))
 print(f"Day 01, part 1: total distance is {total_distance}")
