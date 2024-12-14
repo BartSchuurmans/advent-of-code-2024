b9d5f734c999e54ced19f8cf8e7ca3d4add6b7e3
Bart Schuurmans <bart@minnozz.com>
2024-12-14 12:56:01 +0100

    Day 1 part 2

--- day01/main.py
+++ day01/main.py
@@ -7,3 +7,6 @@ with open(Path(__file__).parent / "input.txt") as f:
 
 total_distance = sum(abs(x - y) for x, y in zip(xs, ys))
 print(f"Day 01, part 1: total distance is {total_distance}")
+
+similarity_score = sum(x * ys.count(x) for x in xs)
+print(f"Day 01, part 2: similarity score is {similarity_score}")
